97f79c6c78b3af519fd3560251ca654afd633e3c
Jose Agudelo <agudelotoxcino@gmail.com>
2024-08-14 18:38:34 -0500

    Auth

--- src/app.py
+++ src/app.py
@@ -1,20 +1,40 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
 from config import config
+
+#Modelos
+from models.ModelUser import ModelUser
+
+#Entities
+from models.entities.User import User
 
 app=Flask(__name__)
 
+db=MySQL(app)
+
 @app.route('/')
 def index():
-    return redirect(url_for('register'))
+    return redirect(url_for('login'))
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def register():
     if request.method=='POST':
-        print(request.form['email'])
-        print(request.form['password'])
-        return render_template('auth/register.html')
+        #print(request.form['email'])
+        #print(request.form['password'])
+        user =User(0,request.form['email'],request.form['password'])
+        logged_user=ModelUser.login(db,user)
+        if logged_user!=None:
+            if logged_user.password:
+                return redirect(url_for('home'))
+        else:
+            flash("Invalid password...")
+            return render_template('auth/register.html')
     else:
-        return render_template('auth/register.html')
+        return render_template('auth/login.html')
+
+@app.route('/home')    
+def home():
+    return render_template('home.html')
 
 if __name__ =='__main__':
     app.config.from_object(config['development'])
--- src/config.py
+++ src/config.py
@@ -1,5 +1,14 @@
-class DelevopmentConfig():
+class Config:
+    SECRET_KEY = 'B!1weNAt1T??kvhUI*S'
+
+
+class DelevopmentConfig(Config):
     DEBUG = True
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'db_system'
+
 
 config = {
     'development': DelevopmentConfig
